42de5e87f67b1c96e57421636fc70b7b0990d389
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-04 15:29:13 -0600

    feat: metodo para actualizar los datos de un empleado

--- src/api/services/AdminService.ts
+++ src/api/services/AdminService.ts
@@ -1,13 +1,22 @@
 import { API_CONFIG } from "../../config/api";
-import type { EmployeeList } from "../../types/types";
+import type { EmployeeList, UpdateEmployee } from "../../types/types";
 import { apiClient } from "../client";
 
 class AdminService {
   private getEmployeesListEndpoint =
     API_CONFIG.endpoints.admin.getEmployeesWithTheirDays;
+  private updateEmployeeEndpoint = API_CONFIG.endpoints.admin.update;
 
   async getEmployeesList(): Promise<EmployeeList[]> {
     return apiClient.get<EmployeeList[]>(this.getEmployeesListEndpoint);
+  }
+
+  async updateEmployee(id: number, data: UpdateEmployee): Promise<void> {
+    const payload = { ...data, id: id };
+    return apiClient.patch<void>(
+      `${this.updateEmployeeEndpoint}${id}`,
+      payload,
+    );
   }
 }
 
