7da81e1188634a1fcb136da6b983c558938616d7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-05 09:27:27 -0600

    feat:metodo para registrar un nuevo empleado

--- src/api/services/AdminService.ts
+++ src/api/services/AdminService.ts
@@ -1,14 +1,23 @@
 import { API_CONFIG } from "../../config/api";
-import type { EmployeeList, UpdateEmployee } from "../../types/types";
+import type {
+  CreateEmployee,
+  EmployeeList,
+  UpdateEmployee,
+} from "../../types/types";
 import { apiClient } from "../client";
 
 class AdminService {
   private getEmployeesListEndpoint =
     API_CONFIG.endpoints.admin.getEmployeesWithTheirDays;
+  private createEmployeeEndpoint = API_CONFIG.endpoints.admin.create;
   private updateEmployeeEndpoint = API_CONFIG.endpoints.admin.update;
 
   async getEmployeesList(): Promise<EmployeeList[]> {
     return apiClient.get<EmployeeList[]>(this.getEmployeesListEndpoint);
+  }
+
+  async createEmployee(data: CreateEmployee): Promise<void> {
+    return apiClient.post<void>(this.createEmployeeEndpoint, data);
   }
 
   async updateEmployee(id: number, data: UpdateEmployee): Promise<void> {
