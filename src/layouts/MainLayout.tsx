c483fd9892bdbf3db365c3b2969d0ff94b0c273e
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 15:26:06 -0600

    feat: usuario y rol

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,40 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Navbar } from "../components/layout/Navbar";
+import toast from "react-hot-toast";
 
 export const MainLayout = () => {
+  const navigate = useNavigate();
+
+  const userRaw = localStorage.getItem("user");
+  const userData = userRaw ? JSON.parse(userRaw) : null;
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    toast.success("Sesión cerrada correctamente", {
+      style: {
+        borderRadius: "12px",
+        background: "#333",
+        color: "#fff",
+      },
+    });
+
+    navigate("/login");
+  };
+
+  const roleDisplay = userData?.role || "Colaborador";
+
   return (
-    <>
-      <Navbar userName="Jose Lugo" role="Admin" />
+    <div>
+      <Navbar
+        userName={userData?.fullName || "Usuario MESA"}
+        role={roleDisplay}
+        onLogout={handleLogout}
+      />
       <div className="container">
         <Outlet />
       </div>
-    </>
+    </div>
   );
 };
