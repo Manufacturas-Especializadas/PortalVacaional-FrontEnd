fa6db57c488bcf1cde8144ac75bdf98cc6e91744
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 15:26:25 -0600

    feat: ruta de configuracion de perfil

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,11 +3,13 @@ import { EmployeeDashboard } from "../pages/EmployeeDashboard/EmployeeDashboard"
 import { Login } from "../pages/Auth/Login/Login";
 import { AdminDashboard } from "../pages/AdminDashboard/AdminDashboard";
 import { MainLayout } from "../layouts/MainLayout";
+import { SetupProfile } from "../pages/SetupProfile/SetupProfile";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/setup-profile" element={<SetupProfile />} />
       <Route element={<MainLayout />}>
         <Route path="/empleado-dashboard" element={<EmployeeDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
