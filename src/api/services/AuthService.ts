3361b951f7cd732b8fc140ccd5eaf22f8fb82855
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 14:14:57 -0600

    feat: metodo login

--- src/api/services/AuthService.ts
+++ src/api/services/AuthService.ts
@@ -1,9 +1,14 @@
 import { API_CONFIG } from "../../config/api";
-import type { SetupProfile } from "../../types/types";
+import type { Login, LoginResponse, SetupProfile } from "../../types/types";
 import { apiClient } from "../client";
 
 class AuthService {
+  private LoginEndpoint = API_CONFIG.endpoints.auth.login;
   private setupProfileEndpoint = API_CONFIG.endpoints.auth.setupProfile;
+
+  async login(data: Login): Promise<LoginResponse> {
+    return apiClient.post<LoginResponse>(this.LoginEndpoint, data);
+  }
 
   async setupProfile(data: SetupProfile): Promise<void> {
     return apiClient.post<void>(this.setupProfileEndpoint, data);
