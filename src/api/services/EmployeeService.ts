c47766f7ca77a1bb1d3fea545dede6564b62964d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-12 14:20:23 -0600

    feat: metodo para solicitar periodo vacacional

--- src/api/services/EmployeeService.ts
+++ src/api/services/EmployeeService.ts
@@ -4,9 +4,19 @@ import { apiClient } from "../client";
 
 class EmployeeService {
   private getDashboardEndpoint = API_CONFIG.endpoints.employee.getDashboard;
+  private requestVacationEndpoint =
+    API_CONFIG.endpoints.employee.requestVacation;
 
   async getDashboard(): Promise<EmployeeDashboardData> {
     return apiClient.get<EmployeeDashboardData>(this.getDashboardEndpoint);
+  }
+
+  async requestVacation(data: {
+    startDate: string;
+    endDate: string;
+    requestedDays: number;
+  }): Promise<void> {
+    return apiClient.post<void>(this.requestVacationEndpoint, data);
   }
 }
 
